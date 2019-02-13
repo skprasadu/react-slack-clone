e18b16488da79fca143408bc815436129f51a8af
Krishna Prasad <skprasadu@yahoo.com>
2019-02-12 20:58:11 -0800

    Implemented login and signout

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
-import * as actionsTypes from './types';
+import * as actionsTypes from "./types";
 
 export const setUser = user => {
-    return {
-        type: actionsTypes.SET_USER,
-        payload: {
-            currentUser: user
-        }
+  return {
+    type: actionsTypes.SET_USER,
+    payload: {
+      currentUser: user
     }
-}
+  };
+};
+
+export const clearUser = () => {
+  return {
+    type: actionsTypes.CLEAR_USER
+  };
+};
